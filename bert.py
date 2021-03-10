fe73023b9db2765dc2cb138dd02a7fb62c90d1e7
root <root@TENCENT64.site>
2021-03-10 19:42:15 +0800

    123

--- bert.py
+++ bert.py
@@ -1,13 +1,15 @@
-#coding=utf-8
+from pytorch_pretrained_bert import BertTokenizer, BertModel
+import numpy as np
 import torch
-from transformers import BertTokenizer, BertModel
-tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
-model = BertModel.from_pretrained("bert-base-chinese")
-inputs = tokenizer("让我们试试BERT模型吧。模型是不是很厉害？", return_tensors="pt")
-print(inputs)
-print(tokenizer.decode(inputs["input_ids"].data.cpu().numpy().reshape(-1)))
-outputs = model(**inputs)
-# 词向量 句子向量
-sequence_outputs, pooled_outputs = outputs
-print(sequence_outputs.shape)
-print(pooled_outputs.shape)
+
+tokenizer = BertTokenizer.from_pretrained("../data/bert-base-uncased/bert-base-uncased-vocab.txt")
+bert = BertModel.from_pretrained("../data/bert-base-uncased/")
+
+s = "I'm not sure, this can work, lol -.-"
+tokens = tokenizer.tokenize(s)
+print("\\".join(tokens))
+ids = torch.tensor([tokenizer.convert_tokens_to_ids(tokens)])
+print(ids.shape)
+result = bert(ids, output_all_encoded_layers=True)
+print(result)
+
